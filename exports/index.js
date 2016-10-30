2256788447f941180531895704773be2cc3585b3
Chris Pollard <cpollard1001@gmail.com>
2016-10-30 07:53:39 -0400

    Simplified exports

--- exports/index.js
+++ exports/index.js
@@ -2,13 +2,6 @@
 
 Storj.Exports = {};
 
-Storj.Exports.createHash = require('create-hash');
-
-var hashes = ['sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'md5', 'rmd160'].concat(Object.keys(require('browserify-sign/algos')))
-Storj.Exports.getHashes = function () {
-  return hashes
-}
-
-var p = require('pbkdf2')
-Storj.Exports.pbkdf2 = p.pbkdf2
-Storj.Exports.pbkdf2Sync = p.pbkdf2Sync
+Storj.Exports.sha256 = require('sha256');
+Storj.Exports.rmd160 = require('ripemd160');
+Storj.Exports.pbkdf2Sync = require('pbkdf2').pbkdf2Sync;
