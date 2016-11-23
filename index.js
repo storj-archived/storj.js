b24a916efb9867ea0f5404b58acc2de3f98111f2
Chris Pollard <cpollard1001@gmail.com>
2016-11-23 16:45:55 -0500

    Added proxy example where server retrieves token of not necessarily public bucket

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 var express = require('express');
+var storj = require('storj-lib');
 var app = express();
 
 var port = 8080;
 
 app.use('/examples', express.static('examples'));
 app.use('/build', express.static('build'));
+
+var client = new storj.BridgeClient( 'https://api.storj.io', {
+  basicAuth: {
+    email: 'email',
+    password: 'password'
+  }
+});
+app.get('/token-proxy', function( req, res, next ){
+  client.createToken( 'ab5517b0232f29778a1d36e0', 'PULL', function( err, token ){
+    token.size = 59477;
+    token.fileId = '635b7c212398a7cdbcfb5b63';
+    res.json( token );
+  });
+});
 
 app.listen(port, function () {
   console.log('Listening on port ' + port);
